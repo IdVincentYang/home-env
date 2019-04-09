33d468e1438637f56f040d2a7246e3382ded6c59
yangws <wensheng.yang@cocos2d-x.org>
2019-04-09 22:28:22 +0800

    [feature] hammerspoon add window resize hotkeys

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,27 +1,77 @@
--- [Getting Started](http:--www.hammerspoon.org/go/)
--- [API Docs](https:--www.hammerspoon.org/docs/)
-local _SUPER_META = { "cmd", "alt", "ctrl", "shift" };
-
---[[--  快捷键：窗口缩放
+--[[--
+--  [Getting Started](http://www.hammerspoon.org/go/)
+--  [API Docs](https://www.hammerspoon.org/docs/)
+--  [Spoons Download](https://www.hammerspoon.org/Spoons/)
 --]]
---[[window resize begin]]
-hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift" }, "0", function()
-    hs.alert.show("全屏");
-    local win = hs.window.focusedWindow();
-    local f = win:frame();
-    local screen = win:screen();
-    local max = screen:frame();
+local _SUPER_META = { "cmd", "alt", "ctrl", "shift" };
+local _log = hs.logger.new("my_config", "debug");
 
-    f.x = max.x;
-    f.y = max.y;
-    f.w = max.w;
-    f.h = max.h;
-    win:setFrame(f);
-end)
---[[window resize end]]
---[[--  快捷键：窗口移动
+--[[--
+--  快捷键：窗口移动
 --]]
 --[[window move begin]]
+local function _get_menu_bar_height()
+    return hs.screen.primaryScreen():frame().y;
+end
+local function _window_move_in_screen(direction, division, duration)
+    local win = hs.window.focusedWindow();
+    if win == nil then
+        hs.alert.show("Error: 没有当前窗口");
+        return;
+    end
+
+    local w = win:frame();
+    local s = win:screen():frame();
+    local dt = _get_menu_bar_height();
+    local inPos = nil;
+
+    -- 窗口在预定位置，连续移动，否则设置为初始位置
+    if (direction == "centered") then
+        local d = hs.geometry.size(math.floor(s.w / division), math.floor(s.h / division));
+        local c = hs.geometry.point(math.floor((s.x + s.w) / 2), math.floor((s.y + s.h) / 2));
+        local isCenter = math.abs(w:distance(c)) < dt;
+        local fitSize = (w.w % d.w < dt) and (w.h % d.h < dt);
+        inPos = isCenter and fitSize;
+        w.size = d:scale(inPos and (math.floor(w.w / d.w) % division + 1) or division);
+        w.center = s.center;
+    elseif (direction == "horizontal") then
+        local d = hs.geometry.size(math.floor(s.w / division), s.h);
+        local fitSize = (math.abs(w.w - d.w) < dt) and (math.abs(w.h - d.h) < dt);
+        inPos = fitSize and ((w.x - s.x) % d.w) == 0;
+        w.x = inPos and ((w.x + d.w) % (d.w * division)) or s.x;
+        w.y = s.y;
+        w.size = d;
+    elseif (direction == "vertical") then
+        local d = hs.geometry.size(s.w, math.floor(s.h / division));
+        local fitSize = (math.abs(w.w - d.w) < dt) and (math.abs(w.h - d.h) < dt);
+        inPos = fitSize and ((w.y - s.y) % d.h) == 0;
+        w.x = s.x;
+        w.y = inPos and ((w.y + d.h) % (d.h * division)) or s.y;
+        w.size = d;
+    elseif (direction == "slash") then
+        local d = hs.geometry.size(math.floor(s.w / division), math.floor(s.h / division));
+        local onGrid = ((w.x - s.x) % d.w) == 0 and ((w.y - s.y) % d.h) == 0;
+        local fitSize = (math.abs(w.w - d.w) < dt) and (math.abs(w.h - d.h) < dt);
+        inPos = onGrid and fitSize and (math.abs(w.x - w.y) > (d.w + d.h) / 2);
+        w.x = inPos and ((w.x - d.w) % (d.w * division)) or (s.x + d.w * (division - 1));
+        w.y = inPos and ((w.y + d.h) % (d.h * division)) or s.y;
+        w.size = d;
+    elseif (direction == "backslash") then
+        local d = hs.geometry.size(math.floor(s.w / division), math.floor(s.h / division));
+        local onGrid = ((w.x - s.x) % d.w) == 0 and ((w.y - s.y) % d.h) == 0;
+        local fitSize = (math.abs(w.w - d.w) < dt) and (math.abs(w.h - d.h) < dt);
+        inPos = onGrid and fitSize and (math.abs(w.x - w.y) < math.min(d.w, d.h) / 2);
+        w.x = inPos and ((w.x + d.w) % (d.w * division)) or s.x;
+        w.y = inPos and ((w.y + d.h) % (d.h * division)) or s.y;
+        w.size = d;
+    else
+        hs.alert.show("Error: 不支持此移动模式："..direction);
+        return;
+    end
+    _log:d("%s{inPos: %s, w: %s}", direction, tostring(inPos), tostring(w));
+    win:setFrameWithWorkarounds(w, duration);
+end
+
 local function _window_move_to_screen(how)
     local win = hs.window.focusedWindow();
     if win == nil then
@@ -44,7 +94,12 @@ local function _window_move_to_screen(how)
 
 end
 
-local _window_move_key_mapping = {
+local _window_move_hotkey_mapping = {
+    window_move_in_center = { _SUPER_META, "1" },
+    window_move_in_screen_horizontal = { _SUPER_META, "2" },
+    window_move_in_screen_vertical = { _SUPER_META, "3" },
+    window_move_in_screen_slash = { _SUPER_META, "4" },
+    window_move_in_screen_backslash = { _SUPER_META, "5" },
     window_move_to_screen_left = { _SUPER_META, "Left" },
     window_move_to_screen_right = { _SUPER_META, "Right" },
     window_move_to_screen_up = { _SUPER_META, "Up" },
@@ -52,11 +107,16 @@ local _window_move_key_mapping = {
 };
 
 local _window_move_action_mapping = {
+    window_move_in_center = hs.fnutils.partial(_window_move_in_screen, "centered", 3, 0),
+    window_move_in_screen_horizontal = hs.fnutils.partial(_window_move_in_screen, "horizontal", 2, 0),
+    window_move_in_screen_vertical = hs.fnutils.partial(_window_move_in_screen, "vertical", 2, 0),
+    window_move_in_screen_slash = hs.fnutils.partial(_window_move_in_screen, "slash", 2, 0),
+    window_move_in_screen_backslash = hs.fnutils.partial(_window_move_in_screen, "backslash", 2, 0),
     window_move_to_screen_left = hs.fnutils.partial(_window_move_to_screen, "Left"),
     window_move_to_screen_right = hs.fnutils.partial(_window_move_to_screen, "Right"),
     window_move_to_screen_up = hs.fnutils.partial(_window_move_to_screen, "Up"),
     window_move_to_screen_down = hs.fnutils.partial(_window_move_to_screen, "Down"),
 };
 
-hs.spoons.bindHotkeysToSpec(_window_move_action_mapping, _window_move_key_mapping);
+hs.spoons.bindHotkeysToSpec(_window_move_action_mapping, _window_move_hotkey_mapping);
 --[[window move end]]
