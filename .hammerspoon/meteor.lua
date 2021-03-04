18235cfb428bd698ecfcb5aef243043736509ec9
yangws <wensheng.yang@cocos2d-x.org>
2021-03-04 16:43:06 +0800

    [update]

---
+++ .hammerspoon/meteor.lua
@@ -0,0 +1,8 @@
+local Class = require("c3class");
+local UnitedHotkey = require("mhs_united_hotkey");
+
+local Meteor = Class("Meteor", {});
+
+Class.Static(Meteor, "UnitedHotkey", UnitedHotkey);
+
+return Meteor;
